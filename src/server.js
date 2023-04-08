48fd1cc0ec42e6f4013cb7b1b0496a508d7b7b3c
Hunter Storm <huntersreese@gmail.com>
2023-04-08 08:28:01 -0600

    update request

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const app = express();
 
 app.get('/hello',(req,res)=>{
-    res.send('welcome!');
+    res.send('welcome to my app!');
 })
 
 app.listen(3000,()=>{
